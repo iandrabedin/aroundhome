9a57fb9288fa12a21189f303f41cbfcbaf2d2765
Iandra Bedin <iandrabedin@gmail.com>
2021-03-05 22:40:09 +0100

    feat: create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import createMockServer from "./services/mock";
+import Home from "./pages/home";
+import NotFound from "./pages/notFound";
 
 if (process.env.NODE_ENV === "development") {
   createMockServer();
@@ -8,22 +10,12 @@ if (process.env.NODE_ENV === "development") {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
