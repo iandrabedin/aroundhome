7f26164ff252903626028308610de6b98263cbb1
Iandra Bedin <iandrabedin@gmail.com>
2021-03-07 23:28:16 +0100

    styles: add responsive breakpoints

--- src/components/column/column.style.ts
+++ src/components/column/column.style.ts
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
 const Column = styled.div`
-  flex: 1;
-  margin: 0 80px;
+  @media (min-width: 992px) {
+    flex: 1;
+    margin: 0 80px;
+  }
 `;
 
 export { Column };
--- src/components/reservation/reservation.style.ts
+++ src/components/reservation/reservation.style.ts
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: row;
   margin: 30px 56px;
+  @media (min-width: 992px) {
+    display: flex;
+    flex-direction: row;
+  }
 `;
 
 export { Container };
