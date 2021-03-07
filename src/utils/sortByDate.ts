e1a1ec328ab98a754884f181fcf1f70d23ce24e3
Iandra Bedin <iandrabedin@gmail.com>
2021-03-07 17:55:14 +0100

    fix: delete unused function sortByDate

--- src/utils/sortByDate.ts
+++ src/utils/sortByDate.ts
@@ -3,7 +3,3 @@ import { TimeSlot } from "types";
 export const sortTimeByNewest = (time: TimeSlot[]) => {
   return time.sort((a, b) => a.end_time.localeCompare(b.end_time));
 };
-
-export const sortTimeByOldest = (time: TimeSlot[]) => {
-  return time.sort((a, b) => a.end_time.localeCompare(b.end_time));
-};
