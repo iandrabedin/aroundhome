1441cbe26e0f0649fbed52f80950d808fe3b7cf2
Iandra Bedin <iandrabedin@gmail.com>
2021-03-07 14:21:20 +0100

    styles: create theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primary: "#005039",
+    secondary: "#e8b000",
+    white: "#FFFFFF",
+    black: "#17171a",
+    grey: "#797980",
+    background: "#e6ebe9",
+  },
+};
+
+export default theme;
